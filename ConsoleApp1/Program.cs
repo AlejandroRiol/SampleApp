977acfe8470bbb97e683c5befd174d921da6a17f
Alejandro Del Rio Ledesma <alejandro.riol@softtek.com>
2021-02-25 21:05:13 -0600

    Init Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                       //0         //1         //2
+            string[] cases = { "progress", "closed", "completed" };
+            Console.WriteLine(cases[0] + " " + cases[1] + " " + cases[2]);
+
+            Random rand = new Random();
+            int index = rand.Next(cases.Length); //0 1 2
+
+            if (cases[index] == "closed")
+            {
+                cases[index] = "progress";
+            }
+
+            Console.WriteLine(cases[0] + " " + cases[1] + " " + cases[2]);
+        }
+    }
+}
